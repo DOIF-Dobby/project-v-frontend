247d260fa56fb130c85930b5c91bb7a35eae7f50
doif-dobby <doif.dobby@gmail.com>
2021-02-22 23:06:10 +0900

    ing sidebar...

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import React, {
   useState,
 } from 'react';
 import './App.css';
-import { theme, GlobalStyle, SideMenu, MenuProps } from 'doif-react-kit';
+import { theme, GlobalStyle, SideMenu, MenuProps, CategoryProps } from 'doif-react-kit';
 import styled, { ThemeProvider } from 'styled-components';
 import 'doif-react-kit/dist/datepicker.css';
 import { Route } from 'react-router-dom';
@@ -20,57 +20,64 @@ import bigLogo from './images/v-logo-big.png';
 function App() {
   const [themeName, setThemeName] = useState('light');
 
-  const menus: Array<MenuProps> = [
+  const items: Array<CategoryProps | MenuProps> = [
     {
       code: 'CATEGORY_01',
       name: '기본정보 관리',
-      sort: 1,
-      depth: 1,
-      type: 'CATEGORY',
       icon: 'heart',
+      childrenItems: [
+        {
+          code: 'CATEGORY_01_01',
+          name: '가맹점 정보',
+          childrenItems: [
+            {
+              code: 'MENU_01',
+              name: '가맹점 정보 관리',
+              url: '/entp1',
+            },
+            {
+              code: 'MENU_02',
+              name: '계약승인처리',
+              url: '/entp2',
+            },
+          ],
+        },
+        {
+          code: 'CATEGORY_01_02',
+          name: '원천사업자 정보',
+          icon: '',
+          childrenItems: [
+            {
+              code: 'MENU_03',
+              name: '원천사업자 정보 관리',
+              url: '/optr1',
+            },
+          ],
+        },
+      ],
     },
     {
-      code: 'CATEGORY_01_01',
-      name: '가맹점 정보',
-      sort: 1,
-      depth: 2,
-      type: 'CATEGORY',
-      icon: '',
+      code: 'CATEGORY_02',
+      name: '개발자 메뉴',
+      icon: 'pencil',
+      childrenItems: [
+        {
+          code: 'MENU_DEV_01',
+          name: '시스템캐시리로드',
+          url: '/dev1',
+        },
+        {
+          code: 'MENU_DEV_02',
+          name: '메뉴 관리',
+          url: '/dev2',
+        },
+      ],
     },
     {
-      code: 'MENU_01',
-      name: '가맹점 정보 관리',
-      sort: 1,
-      depth: 3,
-      type: 'MENU',
-      icon: '',
-      url: '/entp1',
-    },
-    {
-      code: 'MENU_02',
-      name: '계약승인처리',
-      sort: 2,
-      depth: 3,
-      type: 'MENU',
-      icon: '',
-      url: '/entp2',
-    },
-    {
-      code: 'CATEGORY_01_02',
-      name: '원천사업자 정보',
-      sort: 2,
-      depth: 2,
-      type: 'CATEGORY',
-      icon: '',
-    },
-    {
-      code: 'MENU_03',
-      name: '원천사업자 정보 관리',
-      sort: 1,
-      depth: 3,
-      type: 'MENU',
-      icon: '',
-      url: '/optr1',
+      code: 'MENU_04',
+      name: '테스트 메뉴',
+      icon: 'exit',
+      url: '/test',
     },
   ];
 
@@ -81,7 +88,7 @@ function App() {
       <SideMenu
         smallLogo={<img src={smallLogo} />}
         bigLogo={<img src={bigLogo} />}
-        menus={menus}
+        items={items}
       />
 
       <div>
