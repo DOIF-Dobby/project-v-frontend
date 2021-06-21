9b5df7306a4a87b9e84350d37a9f68d08482c5ae
doif-dobby <doif.dobby@gmail.com>
2021-06-21 21:48:37 +0900

    font 추가

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { GlobalStyle, theme as appTheme } from 'doif-react-kit';
 import 'doif-react-kit/dist/doif-react-kit.css';
+import '../style/index.css';
 import React from 'react';
 import { atom, selector, useRecoilValue, useRecoilValueLoadable } from 'recoil';
 import { ThemeProvider } from 'styled-components';
